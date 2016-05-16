d0da0c01b7e30befe48a09eadea55a88b8e06b7a
Willyb15 <william.steen.bryant@gmail.com>
2016-05-16 19:12:40 -0400

    added layer

--- index.php
+++ index.php
@@ -2,18 +2,40 @@
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Manta Site</title>
-</head>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.3/jquery.min.js"></script>
-<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-
-
-
-<body>
-Manta Ray site
-</body>
+	<head>
+		<title>Manta Site</title>
+	</head>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.3/jquery.min.js"></script>
+	<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="css/style.css">
+	<body>
+		<div id="main-wrapper">
+			<nav id="main-nav" class="navbar navbar-inverse">
+				<div class="container-fluid">
+					<div class="left">
+						<button class="btn btn-primary">HOME</button>
+					</div>
+					<div class="right">
+						<button class="btn btn-info">LOGIN</button>
+						<button class="btn btn-warning">REGISTER</button>
+					</div>
+				</div>
+			</nav>
+			<div id="home">
+				<div class="home-text">
+					<p>Defeat the Unicorn Army</p>
+				</div>
+			</div>
+		</div>
+		<div id="mission">
+			<div class="mission-header">
+				<p>Our Misson</p>
+			</div>
+			
+			
+		</div>
+	</body>
 </html>
