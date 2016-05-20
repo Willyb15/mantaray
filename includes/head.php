2ec8f6d09cb22e6fa0451b2cecd12ec4ededf679
Willyb15 <william.steen.bryant@gmail.com>
2016-05-20 12:06:08 -0400

    updated js link

--- includes/head.php
+++ includes/head.php
@@ -19,5 +19,5 @@ session_start();
 	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.9/angular.min.js"></script>
-	<script type="text/javascript" src="../js/controller.js"></script>
+	<script type="text/javascript" src="js/controller.js"></script>
 	<link rel="stylesheet" href="css/style.css">
